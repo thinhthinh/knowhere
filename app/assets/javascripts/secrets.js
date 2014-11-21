f19c02e0f62d583d5928f09dd1d7bace5a6c456d
Shivi <spandey2122@gmail.com>
2014-11-21 14:27:19 -0500

    Added char limit to form

--- app/assets/javascripts/secrets.js
+++ app/assets/javascripts/secrets.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 $('.courses').on('click', '.delete', function(event){
     event.stopPropagation();
     var id = $(this).data('id');
@@ -7,4 +9,20 @@ $('.courses').on('click', '.delete', function(event){
         $('[data-id=' + id + ']').parent('.course').remove()
       }
     )
-  })
+  });
+
+$("#secret_message").keydown(function(){
+  var wordCount = 250-($(this).val().length);
+  $("#count").text(function(){
+    if (wordCount < 0){
+      $(".submit_button").hide();
+    }else {
+      $(".submit_button").show();
+    }
+    return wordCount;
+  });
+});
+
+
+});
+
