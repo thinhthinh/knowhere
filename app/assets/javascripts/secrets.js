1461ed72685493c8a0427fc50976815d79ea1595
Shivi <spandey2122@gmail.com>
2014-11-21 18:34:51 -0500

    add auto-complete

--- app/assets/javascripts/secrets.js
+++ app/assets/javascripts/secrets.js
@@ -22,7 +22,52 @@ $("#secret_message").keydown(function(){
     return wordCount;
   });
 });
-
-
 });
+var autocomplete;
+
+function initialize() {
+  // Create the autocomplete object, restricting the search
+  // to geographical location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      /** @type {HTMLInputElement} */(document.getElementById('secret_address')),
+      { types: ['geocode'] });
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    fillInAddress();
+  });
+}
+// function fillInAddress() {
+//   // Get the place details from the autocomplete object.
+//   var place = autocomplete.getPlace();
+
+//   for (var component in componentForm) {
+//     document.getElementById(component).value = '';
+//     document.getElementById(component).disabled = false;
+//   }
+
+//   // Get each component of the address from the place details
+//   // and fill the corresponding field on the form.
+//   for (var i = 0; i < place.address_components.length; i++) {
+//     var addressType = place.address_components[i].types[0];
+//     if (componentForm[addressType]) {
+//       var val = place.address_components[i][componentForm[addressType]];
+//       document.getElementById(addressType).value = val;
+//     }
+//   }
+// }
+// [START region_geolocation]
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = new google.maps.LatLng(
+          position.coords.latitude, position.coords.longitude);
+      autocomplete.setBounds(new google.maps.LatLngBounds(geolocation,
+          geolocation));
+    });
+  }
+}
+
+
+
 
